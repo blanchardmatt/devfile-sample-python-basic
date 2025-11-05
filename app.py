bd323fb1563168e1e213e0052f35216ad981b202
blanchardmatt <matt.blanchard@gmail.com>
2025-11-05 18:53:30 -0500

    Update hello function return message more

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World! Testing from sc repository!"
+    return "Hello World! Testing from sc repository! Second commit same as the first."
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
